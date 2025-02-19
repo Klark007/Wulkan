16f649246b608aaae26f3e0953d30d3cf06e56e4
Jan Grunder <jgrunder@student.ethz.ch>
2025-02-19 17:36:55 +0100

    Orthographic camera

--- Wulkan/src/engine/Camera.h
+++ Wulkan/src/engine/Camera.h
@@ -29,6 +29,7 @@ public:
 	inline void set_pitch(float pitch);
 	inline void set_near_plane(float near) { z_near = near; };
 	inline void set_far_plane(float far) { z_far = far; };
+	inline void set_orthographic_projection_size(float size) { ortho_size = size; };
 private:
 	glm::vec3 position;
 
@@ -40,6 +41,7 @@ private:
 	float foc_x = -1; // set to invalid values if camera is constructed using fov and not focal length x and y
 	float foc_y = -1;
 	float fov;
+	float ortho_size;
 	float z_near;
 	float z_far;
 
@@ -51,6 +53,7 @@ public:
 	inline glm::mat4 generate_view_mat_LHS() const;
 	inline glm::mat4 generate_virtual_view_mat() const;
 	inline glm::mat4 generate_projection_mat() const;
+	inline glm::mat4 generate_ortho_mat() const;
 	inline glm::vec3 get_pos() const { return position; };
 	inline glm::vec3 get_des() const { return position + get_dir(); };
 	inline glm::vec3 get_dir() const;
@@ -123,6 +126,11 @@ inline glm::mat4 Camera::generate_virtual_view_mat() const
 
 inline glm::mat4 Camera::generate_projection_mat() const {
 	return glm::perspective(fov, get_aspect_ratio(), z_near, z_far);
+}
+
+inline glm::mat4 Camera::generate_ortho_mat() const
+{
+	return glm::ortho(-ortho_size, ortho_size, -ortho_size, ortho_size, z_near, z_far);
 }
 
 // calculate dir given yaw and pitch
